d66bd036fac7b0b1d6e97ba27a583dfec3599eb3
Nirmal Singh <youremail@example.com>
2025-08-06 00:36:59 +0530

    fixing

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const cors=require('cors');
-const dotenv=require('dotenv');
-const connectDB=require('./config/db')
-
-const authRoutes = require("./routes/authRoutes.js");
-const productRoutes = require("./routes/productRoutes");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
-const app=express();
-const PORT=process.env.PORT||5000;
-
-//middleware
 app.use(cors());
 app.use(express.json());
 
+// Connect DB
+const connectDB = require("./config/db");
 connectDB();
 
-app.get('/',(req,res)=>{res.send("E-commerce API running...")});
+// Routes
+const authRoutes = require("./routes/authRoutes.js");
+const productRoutes = require("./routes/productRoutes.js");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-app.listen(PORT ,()=>{
-   console.log(`Server running on http://localhost:${PORT}`);
-})
+// Export instead of listen
+module.exports = app;
+
+const { createServer } = require("@vercel/node");
+
+const server = createServer(app);
+
