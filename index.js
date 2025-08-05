7b9a547c8eb7ed925c22d3759e8dffefb9bc9def
Nirmal Singh <youremail@example.com>
2025-08-06 02:02:32 +0530

    change again

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const dotenv=require('dotenv');
+const connectDB=require('./config/db')
+
+const authRoutes = require("./routes/authRoutes.js");
+const productRoutes = require("./routes/productRoutes");
 
 dotenv.config();
+const app=express();
+const PORT=process.env.PORT||5000;
 
-const app = express();
+//middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("Mongo error:", err));
+connectDB();
 
-// Routes
-app.get("/api/products", (req, res) => {
-  res.json({ message: "Product list would be here." });
-});
+app.get('/',(req,res)=>{res.send("E-commerce API running...")});
 
-app.get('/',(req,res)=>{
-   res.send('E-commerce-api');
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
-// ✅ DO NOT CALL app.listen() — Vercel handles it
-
-app.listen(process.env.PORT,()=>{
-   console.log('Server Started');
-   
+app.listen(PORT ,()=>{
+   console.log(`Server running on http://localhost:${PORT}`);
 })
